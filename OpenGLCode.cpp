446374756ef823aa7dda0eefd53903bce64547e0
ak-iitjm <31209275+ak-iitjm@users.noreply.github.com>
2018-02-14 10:31:32 +0530

    Commit 3 Here, I made it a bit 3-D, but still there is some problem in collision

--- OpenGLCode.cpp
+++ OpenGLCode.cpp
@@ -7,7 +7,7 @@
 #include<time.h>
 using namespace std;
 #define pi 3.14159
-
+#define step 0.005
 class Ball
 {
     GLfloat ballRadius;
@@ -88,47 +88,70 @@ public:
         return ballRadius;
     }
 };
-int Count = 2;
+int count = 3;
 GLfloat ballRadius = 0.1f;
-GLfloat ballX[2],ballY[2],xspeed[2],yspeed[2];
-GLfloat ballXMax = 1, ballYMax = 1, ballXMin = -1 ,ballYMin = -1,r[2];
+GLfloat ballX[3],ballY[3],ballZ[3],xspeed[3],yspeed[3],zspeed[3];
+GLfloat ballXMax = 1, ballYMax = 1, ballXMin = -1 ,ballYMin = -1,ballZMax = -3, ballZMin = -5,r[3];
 GLint refreshmillis = 30;
+GLfloat normal[3];
 GLdouble XLeft,XRight,YTop,YBottom;
 pthread_barrier_t barrier;
 pthread_barrierattr_t attr;
 pthread_mutex_t mutex;
-int ret = pthread_barrier_init(&barrier,&attr,2);
-Ball balls[2];
+int ret = pthread_barrier_init(&barrier,&attr,3);
+Ball balls[3];
 void initGL()
 {
     glClearColor(0.0,0.0,0.0,1);
 }
-GLfloat distance(int i,int j)
+GLfloat sdistance(int i,int j)
 {
-    return(sqrt((ballX[i]-ballX[j])*(ballX[i]-ballX[j])));
+    return((ballX[i]-ballX[j])*(ballX[i]-ballX[j]) + (ballY[i]-ballY[j])*(ballY[i]-ballY[j]) + (ballZ[i]-ballZ[j])*(ballZ[i]-ballZ[j]));
 }
-
+void ncap(int i,int j)
+{
+    normal[0] = (ballX[i] - ballX[j])/ sqrt(sdistance(i,j));
+    normal[1] = (ballY[i] - ballY[j])/ sqrt(sdistance(i,j));
+    normal[2] = (ballZ[i] - ballZ[j])/ sqrt(sdistance(i,j));
+}
+GLfloat vdot(int i)
+{
+    return normal[0]*xspeed[i] + normal[1]*yspeed[i] + normal[2]*xspeed[2];
+}
+void collide(int i,int j)
+{
+    ncap(i,j);
+    GLfloat n1 = vdot(i),n2 = vdot(j);
+    while(sdistance(i,j)<=(r[i]+r[j])*(r[i]+r[j]))
+    {
+        ballX[i]-=step*xspeed[i];
+        ballX[j]-=step*xspeed[j];
+        ballY[i]-=step*yspeed[i];
+        ballY[j]-=step*yspeed[j];
+        ballZ[i]-=step*zspeed[i];
+        ballY[j]-=step*zspeed[j];
+        cout<<sdistance(i,j)<<"Oh Yeah"<<ballX[i]<<" "<<ballX[j]<<endl;
+    }
+    //cout<<"Oh Yeah"<<endl;
+    xspeed[i] = xspeed[i] + (n2 - n1 )*normal[0];
+    yspeed[i] =  yspeed[i] + (n2 - n1 )*normal[1];
+    zspeed[i] =  zspeed[i] + (n2 - n1 )*normal[2];
+    xspeed[j] =  xspeed[j] + (n1 - n2 )*normal[0];
+    yspeed[j] =  yspeed[j] + (n1 - n2 )*normal[1];
+    zspeed[j] =  zspeed[j] + (n1 - n2 )*normal[2];
+}
 void display()
 {	
     glClear(GL_COLOR_BUFFER_BIT);
-    for(int j=0;j<2;j++)
+    glEnable(GL_DEPTH_TEST);
+    for(int j=0;j<3;j++)
     {
         glMatrixMode(GL_MODELVIEW);
     	glLoadIdentity();
-    	//glTranslatef(ballX[j],ballY[j],0.0f);
-    	glBegin(GL_TRIANGLE_FAN);
-    	glColor3f(0.0f,1.0f,0.0f);
-    	glVertex2f(ballX[j],ballY[j]);
-        cout<<ballX[j]<<" "<<ballY[j]<<" "<<j<<endl;
-        cout<<r[j]<<endl;
-    	GLint numSegments =  60;
-    	GLfloat angle;
-    	for(int i =0;i<=numSegments;i++)
-    	{
-        	angle = i * 2.0f * pi / numSegments;
-        	glVertex2f(ballX[j]+cos(angle)*r[j], ballY[j]+ sin(angle) * r[j]); 
-    	}
-       // cout<<ballX[j]<<" "<<ballY[j]<<endl;
+    	glTranslatef(ballX[j],ballY[j],ballZ[j]);
+        glColor3f(0.9, 0.3, 0.2);
+        glScalef(1.0,1.0,1.0);
+        glutSolidSphere(r[j],20,20);
     	glEnd();
     }
     glutSwapBuffers();
@@ -157,6 +180,7 @@ void reshape(GLsizei width,GLsizei height)
         YTop = 1.0 / aspect;
     }
     gluOrtho2D(XLeft,XRight,YBottom,YTop);
+    gluPerspective(39.0,(GLdouble)width/(GLdouble)height,1,6);
     ballXMin = XLeft + ballRadius;
     ballXMax = XRight - ballRadius;
     ballYMin = YBottom + ballRadius;
@@ -179,17 +203,19 @@ void *bball(void* j)
 	int i = (long long int) j;
     ballX[i] = balls[i].get_x();
     ballY[i] = balls[i].get_y();
+    ballZ[i] = balls[i].get_z();
     xspeed[i] = balls[i].get_vx();
     yspeed[i] = balls[i].get_vy();
+    zspeed[i] = balls[i].get_vz();
     r[i] = balls[i].get_radius();
-    pthread_barrier_wait(&barrier);
 	while(true)
 	{	
         ballX[i]+=xspeed[i];
     	ballY[i]+=yspeed[i];
+        ballZ[i]+=zspeed[i];
     	if(ballX[i]>ballXMax)
     	{
-        	xspeed[i] = ballXMax;
+        	ballX[i] = ballXMax;
         	xspeed[i] = -xspeed[i];
     	}
     	else if(ballX[i]<ballXMin)
@@ -207,19 +233,28 @@ void *bball(void* j)
         	ballY[i] = ballYMin;
         	yspeed[i] = -yspeed[i];
     	}
+        if(ballZ[i]>ballZMax)
+        {
+            ballZ[i] = ballZMax;
+            zspeed[i] = -zspeed[i];
+        }
+        else if(ballZ[i]<ballZMin)
+        {
+            ballZ[i] = ballZMin;
+            zspeed[i] = -zspeed[i];
+        }
         pthread_barrier_wait(&barrier);
         pthread_mutex_lock(&mutex);
-        for(int k = i+1;k<2;k++)
+        for(int k = i+1;k<3;k++)
         {
-            if(distance(i,k)<r[i]+r[j])
+            if(sdistance(i,k)<=(r[i]+r[k])*(r[i]+r[k]))
             {
-                GLfloat l = xspeed[i];
-                xspeed[i] = xspeed[k];
-                xspeed[k] = l;
+                collide(i,k);
+                //cout<<"Yeah"<<endl;
             }
         }
         pthread_mutex_unlock(&mutex);
-        usleep(10000);
+        usleep(20000);
     }
     
 }
@@ -238,17 +273,30 @@ int main(int argc,char** argv)
     pthread_t id[2];
     int j = 0;
     balls[0].set_x(0.3);
-    balls[0].set_y(0.5);
+    balls[0].set_y(0.4);
+    balls[0].set_z(0);
     balls[1].set_x(-0.3);
-    balls[1].set_y(-0.5);
+    balls[1].set_y(0.4);
+    balls[1].set_z(0.2);
+    balls[2].set_x(0);
+    balls[2].set_y(0.5);
+    balls[2].set_z(-0.2);
     balls[0].set_radius(0.1);
     balls[1].set_radius(0.1);
-    balls[0].set_vx(0.01);
-    balls[1].set_vx(-0.01);
-    balls[0].set_vy(0.01);
-    balls[1].set_vy(-0.01);
+    balls[2].set_radius(0.1);
+    balls[0].set_vx(-0.03);
+    balls[1].set_vx(0.03);
+    balls[2].set_vx(0);
+    balls[0].set_vy(-0.04);
+    balls[1].set_vy(-0.04);
+    balls[2].set_vy(-0.05);
+    balls[0].set_vz(-0.02);
+    balls[1].set_vz(0.02);
+    balls[2].set_vz(0.06);
     pthread_create(&id[0],NULL,bball,(void *) j);
     j = 1;
+    pthread_create(&id[1],NULL,bball,(void *) j);
+    j = 2;
     pthread_create(&id[1],NULL,bball,(void *) j);
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
