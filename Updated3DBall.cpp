c325759b85e4c0c60cbfd1e6d696772075a2b97f
abhi1607IIT <2016ucsj0012@iitjammu.ac.in>
2018-02-15 12:41:16 +0530

    added box to updated3dball

--- Updated3DBall.cpp
+++ Updated3DBall.cpp
@@ -140,10 +140,84 @@ void collide(int i,int j)
         ballY[j]+=step*zspeed[j];
     }
 }
+
+void DrawCube(void)
+{
+
+     glMatrixMode(GL_MODELVIEW);
+    // clear the drawing buffer.
+    glClear(GL_COLOR_BUFFER_BIT);
+   glLoadIdentity();
+        glTranslatef(0.0,0.0,0.0);
+    //glRotatef(xRotated,1.0,0.0,0.0);
+    // rotation about Y axis
+    //glRotatef(yRotated,0.0,1.0,0.0);
+    // rotation about Z axis
+   // glRotatef(zRotated,0.0,0.0,1.0);
+  glBegin(GL_QUADS);        // Draw The Cube Using quads
+    glColor3f(0.0f,1.0f,0.0f);    // Color Blue
+    glVertex3f( XRight,YTop,ZBack);    // Top Right Of The Quad (Top)
+    glVertex3f(XLeft,YTop,ZBack);    // Top Left Of The Quad (Top)
+    glVertex3f(XLeft,YTop,ZFront);    // Bottom Left Of The Quad (Top)
+    glVertex3f( XRight,YTop,ZFront);    // Bottom Right Of The Quad (Top)
+    glColor3f(1.0f,0.5f,0.0f);    // Color Orange
+    glVertex3f(XRight,YBottom,ZFront);    // Top Right Of The Quad (Bottom)
+    glVertex3f(XLeft,YBottom,ZFront);    // Top Left Of The Quad (Bottom)
+    glVertex3f(XLeft,YBottom,ZBack);    // Bottom Left Of The Quad (Bottom)
+    glVertex3f(XRight,YBottom,ZBack);    // Bottom Right Of The Quad (Bottom)
+    glColor3f(1.0f,0.0f,0.0f);    // Color Red    
+    glVertex3f(XRight,YTop,ZFront);    // Top Right Of The Quad (Front)
+    glVertex3f(XLeft,YTop,ZFront);    // Top Left Of The Quad (Front)
+    glVertex3f(XLeft,YBottom,ZFront);    // Bottom Left Of The Quad (Front)
+    glVertex3f(XRight,YBottom,ZFront);    // Bottom Right Of The Quad (Front)
+    glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
+    glVertex3f(XRight,YBottom,ZBack);    // Top Right Of The Quad (Back)
+    glVertex3f(XLeft,YBottom,ZBack);    // Top Left Of The Quad (Back)
+    glVertex3f(XLeft,YTop,ZBack);    // Bottom Left Of The Quad (Back)
+    glVertex3f(XRight,YTop,ZBack);    // Bottom Right Of The Quad (Back)
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glVertex3f(XLeft,YTop,ZFront);    // Top Right Of The Quad (Left)
+    glVertex3f(XLeft,YTop,ZBack);    // Top Left Of The Quad (Left)
+    glVertex3f(XLeft,YBottom,ZBack);    // Bottom Left Of The Quad (Left)
+    glVertex3f(XLeft,YBottom,ZFront);    // Bottom Right Of The Quad (Left)
+    glColor3f(1.0f,0.0f,1.0f);    // Color Violet
+    glVertex3f(XRight,YTop,ZBack);    // Top Right Of The Quad (Right)
+    glVertex3f(XRight,YTop,ZFront);    // Top Left Of The Quad (Right)
+    glVertex3f(XRight,YBottom,ZFront);    // Bottom Left Of The Quad (Right)
+    glVertex3f(XRight,YBottom,ZBack);    // Bottom Right Of The Quad (Right)
+  glEnd();            // End Drawing The Cube
+glFlush();
+}
+
+/*
+void animation(void)
+{
+ 
+     yRotated += 0.01;
+     xRotated += 0.02;
+    DrawCube();
+}
+
+*/
+void reshapebox(int x, int y)
+{
+    if (y == 0 || x == 0) return;  //Nothing is visible then, so return
+    //Set a new projection matrix
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity();
+    //Angle of view:45 degrees
+    //Near clipping plane distance: 0.5
+    //Far clipping plane distance: 20.0
+     
+    gluPerspective(45.0,(GLdouble)x/(GLdouble)y,0.5,20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0,0,x,y);  //Use the whole window for rendering
+}
 void display()
 {	
     glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
+    DrawCube();
     for(int j=0;j<3;j++)
     {
         glMatrixMode(GL_MODELVIEW);
